623779e34e0fd8ff65a754de554a856047464c09
cameron boyle <cameronboyle7894@gmail.com>
2022-06-22 15:38:29 +0100

    added load end scene line to health script

--- CampRaptureMiniGame/Assets/Scripts/PlayerHealth.cs
+++ CampRaptureMiniGame/Assets/Scripts/PlayerHealth.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
+
+    public string gameOverScene;
     // This function is NOT built in to Unity
     // It will only be called manually by our own code
     // It must be marked "public" so our other scripts can access it
     public void Kill()
     {
         // This will destroy the gameObject that this script is attached to
-        Destroy(gameObject);
+        //Destroy(gameObject);
+        SceneManager.LoadScene(gameOverScene);
     }
 }
